3ee984c4180c80cfa39c08fccf767164633e8780
svoncoelln <89931312+svoncoelln@users.noreply.github.com>
2021-11-22 15:35:43 -0500

    Add files via upload

--- Noah.java
+++ Noah.java
@@ -1,10 +1,22 @@
 public class Noah {
 	
-	public Noah() {
+	public Noah(int r, int c) {
+
+	}
+	
+	public void findSafeMove(int row, int col, Labyrinth l) {
 		
 	}
 	
-	public int[] solve(Labyrinth l) {
+	public boolean isASafeMove(int row, int col, int[] x, Labyrinth l) {
+		
+	}
+	
+	public int[][] solve(Labyrinth l) {
+		
+	}
+	
+	public static void main(String args[]) {
 		
 	}
 }
