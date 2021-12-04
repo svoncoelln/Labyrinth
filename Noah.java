99c14a606296bc6b40843b7a4bebceea2bc2257a
svoncoelln <svoncoelln@friendsbalt.org>
2021-12-04 16:53:28 -0500

    works and commented

--- Noah.java
+++ Noah.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
 
+/**
+* Recursively solve a randomly generated labyrinth and stores the moves made
+* to accomplish this in an ArrayList. Convert the ArrayList to an int[] array
+* where: <ul> 
+* <li> 0 represents a move up </li>
+* <li> 1 represents a move down </li>
+* <li> 2 represents a move left </li>
+* <li> 3 represents a move right </li>
+* </ul>
+*/ 
 public class Noah {
 
 	static int totalCols; // number of rows in the maze, randomly generated between 5 and 15
@@ -16,6 +26,7 @@ public class Noah {
 	* references to arrays representing the labyrinth. 
 	* @param col the current "column" in the labyrinth, used as the second index
 	* in references to arrays representing the labyrinth.
+	* @param l the labyrinth to be solved.
 	* @return a boolean representing whether the algorithm has been to the last
 	* square in the maze. 
     */
@@ -31,8 +42,6 @@ public class Noah {
 				if (findSafeMove(row + 1, col, l)) {
 					return true;
 				}
-				directions.remove(directions.size()-1);
-				visited[row][col] = false;
 			}
 
 			if (isSafe(row, col, Labyrinth.RIGHT, l)) {
@@ -40,8 +49,6 @@ public class Noah {
 				if (findSafeMove(row, col + 1, l)) {
 					return true;
 				}
-				directions.remove(directions.size()-1);
-				visited[row][col] = false;
 			}
 
 			if (isSafe(row, col, Labyrinth.UP, l)) {
@@ -49,8 +56,6 @@ public class Noah {
 				if (findSafeMove(row - 1, col, l)) {
 					return true;
 				}
-				directions.remove(directions.size()-1);
-				visited[row][col] = false;
 			}
 
 			if (isSafe(row, col, Labyrinth.LEFT, l)) {
@@ -58,16 +63,17 @@ public class Noah {
 				if (findSafeMove(row, col - 1, l)) {
 					return true;
 				}
-				directions.remove(directions.size()-1);
-				visited[row][col] = false;
 			}
+			directions.remove(directions.size()-1);
+			visited[row][col] = false;
 		}
 		return false;
 	}
 	
 	/**
 	* Call findSafeMove to solve the labyrinth. Convert the arraylist of moves
-	* made to an array of Integers and convert that array to an array of ints. 
+	* made to an array of Integers, then convert that array to an array of ints
+	* and return it. 
 	*
 	* @param l the labyrinth to be solved.
 	* @return an int array containing the moves made to solve the maze. 
@@ -90,7 +96,7 @@ public class Noah {
 	* Determine if a move is in the labyrinth, and if so, check if it is stone
 	* and if the algorithm has visited it previously. 
 	* 
-	* @param the current "row" in the labyrinth, used as the first index in
+	* @param row the current "row" in the labyrinth, used as the first index in
 	* references to arrays representing the labyrinth. 
 	* @param col the current "column" in the labyrinth, used as the second index
 	* in references to arrays representing the labyrinth.
